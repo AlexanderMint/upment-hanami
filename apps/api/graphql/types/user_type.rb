be9306cff02d87aa870aa82705b9b1daac3788f8
AlexanderMint <webinfo.alexander@gmail.com>
2017-09-15 11:55:50 +0300

    Fix graphql field name

--- apps/api/graphql/types/user_type.rb
+++ apps/api/graphql/types/user_type.rb
@@ -6,8 +6,8 @@ module Types
     description 'A User'
 
     field :id, types.ID, 'The id of the User'
-    field :first_name, types.String, 'The first_name of the User'
-    field :last_name, types.String, 'The last_name of the User'
+    field :firstName, types.String, 'The first_name of the User', property: :first_name
+    field :lastName, types.String, 'The last_name of the User', property: :last_name
     field :email, types.String, 'The id email the User'
   end
 end
