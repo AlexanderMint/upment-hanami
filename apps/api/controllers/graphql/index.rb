d302b37f5250f366098e4e7657b1d38ef95a1e27
AlexanderMint <webinfo.alexander@gmail.com>
2017-10-03 13:31:53 +0300

    Added root graphql validations

--- apps/api/controllers/graphql/index.rb
+++ apps/api/controllers/graphql/index.rb
@@ -4,7 +4,14 @@ module Api::Controllers::Graphql
   class Index
     include Api::Action
 
+    params do
+      required(:query)  { filled? & str? }
+      optional(:vairables)  { str? }
+    end
+
     def call(params)
+      return status 400, 'Invalid data' unless params.valid?
+
       status 200, Schema.execute(params[:query],
                                  variables: params[:vairables] || {},
                                  context: { user_id: session[:user_id] }).to_json
