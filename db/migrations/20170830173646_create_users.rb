a8ee0a35dd9444d51446e0f63d5b82562101205e
AlexanderMint <webinfo.alexander@gmail.com>
2017-10-10 17:02:36 +0300

    Fix users migration

--- db/migrations/20170830173646_create_users.rb
+++ db/migrations/20170830173646_create_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Hanami::Model.migration do
-  change do
+  up do
     create_table :users do
       primary_key :id
 
@@ -17,5 +17,9 @@ Hanami::Model.migration do
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
     end
+  end
+
+  down do
+    drop_table :users
   end
 end
