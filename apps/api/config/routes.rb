6e15ae27660cc562e0f8e00ce789633c338e30ae
AlexanderMint <webinfo.alexander@gmail.com>
2017-10-16 16:05:12 +0300

    Remove garbage

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
-get '/graphql', to: 'graphql#index'
 post '/graphql', to: 'graphql#index'
 options '/graphql', to: 'graphql#index'
--- apps/api/graphql/types/query_type.rb
+++ apps/api/graphql/types/query_type.rb
@@ -4,10 +4,10 @@ module Types
   QueryType = GraphQL::ObjectType.define do
     name 'Query'
 
-    # http://up.com/graphql?query={user(id: 1){ id, first_name, last_name }}
+    # /graphql?query={user(id: 1){ id, first_name, last_name }}
     field :user, Fetch::Field.build(model: UserRepository, type: UserType)
 
-    # http://up.com/graphql?query={users{ id, first_name, last_name }}
+    # /graphql?query={users{ id, first_name, last_name }}
     field :users, Fetch::Collection.build(model: UserRepository, type: UserType)
   end
 end
